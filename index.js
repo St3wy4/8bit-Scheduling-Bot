206822f99ad5cc28d390c33b79b28a5db4e30eca
St3wy4 <thomasskeens004@gmail.com>
2026-05-04 08:24:33 -0400

    fix require path

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(./commands/${file});
+  const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
